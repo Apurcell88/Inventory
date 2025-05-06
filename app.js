0657a0777512bf9adda574a9f26ef7d85100d776
Adam Purcell <apurcell88@gmail.com>
2025-05-06 16:36:44 -0500

    Fixed a memory leak

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
 const gamesRouter = require("./routes/gamesRouter");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: "surfing dog",
+  })
+);
 app.use(
   session({
     secret: "my-secret-key",
